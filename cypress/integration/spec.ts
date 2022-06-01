e87ab18a783dcd95bcebae15e2026b585d447ae3
William Pavei Antero <williamantero@gmail.com>
2022-06-01 02:03:51 +0000

    Visits the meetup page (RED)

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -4,4 +4,9 @@ describe('My First Test', () => {
     cy.contains('Welcome')
     cy.contains('meetup-tdd app is running!')
   })
+
+  it('Visits the meetup page', () => {
+    cy.visit('meetup')
+    cy.contains('Esta é uma página de demonstração para o meetup de TDD!')
+  })
 })
