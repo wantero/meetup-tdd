0814dc173e4240caf39afa4893f150f948090baf
William Pavei Antero <williamantero@gmail.com>
2022-06-01 01:26:04 +0000

    Add meetup component

---
+++ src/app/meetup/meetup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meetup',
+  templateUrl: './meetup.component.html',
+  styleUrls: ['./meetup.component.scss']
+})
+export class MeetupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
